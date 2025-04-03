248b8b539447d54a0eec71a4e15ee8d2ad9c269c
vladiguive <vladimirguivenal@gmail.com>
2025-04-03 11:22:10 -0300

    update geoserver version

--- kubernete/microk8s/install.sh
+++ kubernete/microk8s/install.sh
@@ -3,34 +3,34 @@ source .env
 
 echo "Applying .env config"
 
-grep -rl KUBERNETES-SITE-URL --exclude install.sh . | xargs sed -i 's|KUBERNETES-SITE-URL|'"$KUBERNETES_SITE_URL"'|g'
-grep -rl KUBERNETES-NODE-NAME --exclude install.sh . | xargs sed -i 's|KUBERNETES-NODE-NAME|'"$KUBERNETES_NODE_NAME"'|g'
-grep -rl KUBERNETES-VOL-DIR --exclude install.sh . | xargs sed -i 's|KUBERNETES-VOL-DIR|'"$KUBERNETES_VOL_DIR"'|g'
-grep -rl CLUSTER-ISSUER-NAME --exclude install.sh . | xargs sed -i 's|CLUSTER-ISSUER-NAME|'"$CLUSTER_ISSUER_NAME"'|g'
-grep -rl KUBERNETES-NAMESPACE --exclude install.sh . | xargs sed -i 's|KUBERNETES-NAMESPACE|'"$KUBERNETES_NAMESPACE"'|g'
+cp -r ./templates workdir
 
-sed -i 's|SERVER-PUBLIC-IP|'"$SERVER_PUBLIC_IP"'|g' ./configs/loadbalancer/metallb-configmap.yaml 
-sed -i 's|GEOSERVER_ADMIN_PASSWORD: geoserver|GEOSERVER_ADMIN_PASSWORD: '"$GEOSERVER_PASSWORD"'|g' ./gn-cloud/configmaps/geonode-configmap.yaml
+grep -rl KUBERNETES-SITE-URL ./workdir | xargs sed -i 's|KUBERNETES-SITE-URL|'"$KUBERNETES_SITE_URL"'|g'
+grep -rl KUBERNETES-NODE-NAME ./workdir | xargs sed -i 's|KUBERNETES-NODE-NAME|'"$KUBERNETES_NODE_NAME"'|g'
+grep -rl KUBERNETES-VOL-DIR ./workdir | xargs sed -i 's|KUBERNETES-VOL-DIR|'"$KUBERNETES_VOL_DIR"'|g'
+grep -rl CLUSTER-ISSUER-NAME ./workdir | xargs sed -i 's|CLUSTER-ISSUER-NAME|'"$CLUSTER_ISSUER_NAME"'|g'
+grep -rl KUBERNETES-NAMESPACE ./workdir | xargs sed -i 's|KUBERNETES-NAMESPACE|'"$KUBERNETES_NAMESPACE"'|g'
+grep -rl ACL-PASWORD ./workdir | xargs sed -i 's|ACL-PASWORD|'"$ACL_PASSWORD"'|g'
+
+sed -i 's|SERVER-PUBLIC-IP|'"$SERVER_PUBLIC_IP"'|g' ./workdir/configs/loadbalancer/metallb-configmap.yaml 
+
 mkdir  $KUBERNETES_VOL_DIR/dbdata/
 mkdir  $KUBERNETES_VOL_DIR/geowebcache-data/
-mkdir  $KUBERNETES_VOL_DIR/pgconfigdb-data/
 mkdir  $KUBERNETES_VOL_DIR/rabbitmq-data/
-mkdir  $KUBERNETES_VOL_DIR/statics/
-mkdir  $KUBERNETES_VOL_DIR/tmp/
 
 microk8s enable metallb:$SERVER_PUBLIC_IP-$SERVER_PUBLIC_IP
-microk8s kubectl apply -R -f configs
-microk8s kubectl apply -R -f database
+microk8s kubectl apply -R -f workdir/configs
+microk8s kubectl apply -R -f workdir/database
 
 echo "Waiting for database to start"
 
-tempgndb=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=gndatabase --no-headers`
+tempgndb=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=database --no-headers`
 gnbd=( $tempgndb )
 gnbdstatus=${gnbd[2]}
 
 while [ "$gnbdstatus" != "Running" ]
 do
-    tempgndb=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=gndatabase --no-headers`
+    tempgndb=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=database --no-headers`
     gnbd=( $tempgndb )
     gnbdstatus=${gnbd[2]}
 done
@@ -38,37 +38,35 @@ sleep 5
 
 echo "Applying initial database configuration"
 
-gndatabase=${gnbd[0]}
+database=${gnbd[0]}
 
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE ROLE geonode WITH PASSWORD 'geonode'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE ROLE geonode_data WITH PASSWORD 'geonode_data'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE ROLE pgconfig WITH PASSWORD 'pgconfig'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE geonode WITH login";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE geonode WITH superuser";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE geonode_data WITH login";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE geonode_data WITH superuser";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE pgconfig WITH login";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "ALTER ROLE pgconfig WITH superuser";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE DATABASE geonode WITH OWNER 'geonode'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE DATABASE geonode_data WITH OWNER 'geonode_data'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres postgres -c "CREATE DATABASE pgconfig WITH OWNER 'pgconfig'";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres geonode -c "CREATE EXTENSION postgis";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres geonode_data -c "CREATE EXTENSION postgis";
-microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $gndatabase -- psql -U postgres pgconfig -c "CREATE EXTENSION postgis";
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'CREATE ROLE pgconfig WITH PASSWORD '"$ACL_PASSWORD"'';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'ALTER ROLE pgconfig WITH login';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'ALTER ROLE pgconfig WITH superuser';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'CREATE DATABASE pgconfig OWNER pgconfig';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres pgconfig -c 'CREATE EXTENSION postgis';
+
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'CREATE ROLE '"$DATABASE_USER"' WITH PASSWORD '"$DATABASE_PASS"'';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'ALTER ROLE '"$DATABASE_USER"' WITH login';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'ALTER ROLE '"$DATABASE_USER"' WITH superuser';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres postgres -c 'CREATE DATABASE '"$DATABASE_NAME"' OWNER '"$DATABASE_USER"'';
+microk8s kubectl exec -n $KUBERNETES_NAMESPACE -it $database -- psql -U postgres $DATABASE_NAME -c 'CREATE EXTENSION postgis';
+
+
 
 echo "Starting remaining services"
 
-microk8s kubectl apply -R -f gs-cloud
+microk8s kubectl apply -R -f workdir/geoserver-cloud
 
-tempdjango=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=django --no-headers`
-django=( $tempdjango )
-djangostatus=${django[2]}
+tempwebui=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=webui --no-headers`
+webui=( $tempwebui )
+webuistatus=${webui[2]}
 
-while [ "$djangostatus" != "Running" ]
+while [ "$webuistatus" != "Running" ]
 do
-    tempdjango=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=django --no-headers`
-    django=( $tempdjango )
-    djangostatus=${django[2]}
+    tempwebui=`microk8s kubectl get pod -n $KUBERNETES_NAMESPACE -l component=webui --no-headers`
+    webui=( $tempwebui )
+    webuistatus=${webui[2]}
 done
 
 echo "Done."
--- kubernete/microk8s/uninstall.sh
+++ kubernete/microk8s/uninstall.sh
@@ -3,9 +3,6 @@ source .env
 
 rm -rf  $KUBERNETES_VOL_DIR/dbdata/
 rm -rf  $KUBERNETES_VOL_DIR/geowebcache-data/
-rm -rf  $KUBERNETES_VOL_DIR/pgconfigdb-data/
 rm -rf  $KUBERNETES_VOL_DIR/rabbitmq-data/
-rm -rf  $KUBERNETES_VOL_DIR/statics/
-rm -rf  $KUBERNETES_VOL_DIR/tmp/
 
-microk8s kubectl delete -R -f .
+microk8s kubectl delete -R -f ./workdir
